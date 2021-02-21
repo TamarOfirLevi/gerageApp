92177e0f6443931f7f4c8ad586c1f7a83e58ed85
TamarOfirLevi <tamaro18618@gmail.com>
2021-02-22 00:31:47 +0200

    change for get_all _worker- view employee points

--- src/app/component/viewing-mishaps/viewing-mishaps.component.ts
+++ src/app/component/viewing-mishaps/viewing-mishaps.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { mishap } from '../classes';
+import {MishapServiceService } from 'src/app/services/mishap-service.service';
 
 @Component({
   selector: 'app-viewing-mishaps',
   templateUrl: './viewing-mishaps.component.html',
-  styleUrls: ['./viewing-mishaps.component.scss']
+  styleUrls: ['./viewing-mishaps.component.scss'],
+  providers : [
+    mishap
+ ]
 })
 export class ViewingMishapsComponent implements OnInit {
+  public mishap:mishap;
+  public mishaps:mishap[];
+  constructor( private mishapserv: MishapServiceService) { }
+  getMishaps(){
+    this.mishapserv.getMishaps().subscribe(result => {
+      console.log(result);
+      this.mishaps = result as mishap[];
+    })
+  }
+  getMishapsByCode(code){
+    this.mishapserv.getMishapsByCode(code).subscribe(result => {
+      console.log(result);
+      this.mishap = result as mishap;
+    })
+  }
+  deleteMishapByCode(code){
+    this.mishapserv.deleteMishapByCode(code);
+  }
 
-  constructor() { }
-
+  
   ngOnInit() {
+    this.getMishaps();
+    console.log(this.mishap);
   }
 
 }
